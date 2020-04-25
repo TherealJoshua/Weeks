5875c048b88946d937b2fa01501543ab76d851c3
TherealJoshua <5932JoshuaJ*>
2020-04-25 13:13:52 -0700

    Rotate Attempt 1
    This is my first attempt.

--- Gameheads Unity First Game/Assets/Scripts/Follow.cs
+++ Gameheads Unity First Game/Assets/Scripts/Follow.cs
@@ -7,6 +7,7 @@ public class Follow : MonoBehaviour
     public GameObject target;
     public float followDistance;
     public float cameraFollowSpeed = 0.1f;
+    public Vector3 speed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,8 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 currentSpeed = Vector3.zero;
+        
         Vector3 targetPosition = target.transform.position;
 
         Vector3 followVector = transform.forward * -followDistance;
@@ -28,5 +31,22 @@ public class Follow : MonoBehaviour
         Vector3 cameraTargetPosition = targetPosition + followVector;
 
         transform.position = Vector3.Lerp(transform.position, cameraTargetPosition, cameraFollowSpeed * Time.deltaTime);
+
+         Vector3 targetDir = target.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.up);
+        
+        if (angle < 5.0f)
+        {
+            print("close");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+       {
+           currentSpeed.x = -speed.x;
+       }
+        if (Input.GetKey(KeyCode.RightArrow))
+       {
+           currentSpeed.x = speed.x;
+       }
     }
+        
 }
