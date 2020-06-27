98e8990c5ba13005bd41906dff4b0d57c90fac92
TherealJoshua <joshjerez09gmail.com>
2020-06-27 15:11:07 -0700

    Final Class
    Final Class of Raymonds amazing class.

---
+++ Gameheads Unity First Game/Assets/Scripts/GemSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject gemPrefab;
+    private GameObject gemObject;
+    public Mesh objectMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        if (gemObject == null)
+        {
+              gemObject = GameObject.Instantiate(gemPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+        Gizmos.DrawMesh(objectMesh);
+    }
+}
--- Gameheads Unity First Game/Assets/Scripts/TwoDMove.cs
+++ Gameheads Unity First Game/Assets/Scripts/TwoDMove.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class TwoDMove : MonoBehaviour
 {
-    public Vector3 speed;
-    private float currentSpeed = 0.0f;
+    public float speed = 5;
+    public float jumpSpeed = 2.0f;
+    private Rigidbody2D rb;
+    private float distanceToGround = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        distanceToGround = GetComponent<CircleCollider2D>().bounds.extents.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    bool IsGrounded()
     {
-        if (Input.GetKey(KeyCode.A))
+        return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, distanceToGround + 0.1f);
+        // return (Mathf.Abs(rb.velocity.y) < Mathf.Epsilon)
+    }
+
+// Update is called once per frame
+void FixedUpdate()
+    {
+        float currentSpeed = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            currentSpeed = speed.x;
+            currentSpeed -= speed;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            currentSpeed = speed.x;
+            currentSpeed += speed;
         }
-        if (Input.GetKey(KeyCode.W))
+
+        rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
+
+        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
         {
-            currentSpeed = speed.y;
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
     }
 }
